87ef33890aefc7f01e84c55eaa3e2259dc854503
SimpleSounds VST <tester.nick79@gmail.com>
2019-01-07 16:55:01 -0800

    - WELCOME USER √ DONE - USER ENTERS GUESSING GAME BY PRINTING HEADER √ DONE - WHILE LOOP FOR GAME PLAY 1 TIME with:     * logic to print magic_number, my_guess, trys √ DONE     * when user guess correctly √ DONE     * prompts user if to high or to low √ DONE
    - TODO STEP 4 ??

---
+++ PythonGuessingGame/game_final-V5.py
@@ -0,0 +1,78 @@
+import random
+
+def print_welcome():
+    # 1. Welcome the user to the game
+    print("""
+    ===============================================
+            Welcome to the Guessing Game
+    ===============================================""")
+
+def print_guessing_game():
+    print("""
+    ********************   GUESSING GAME   ********************\n
+    Enter a number between 1 and 8:   """)
+
+
+def prompt_user():
+    '''- prompt the user for a number, Handle invalid values
+       - Store a message, return a stored value entered
+    '''
+    while True:
+
+          try:
+            guess = int(input())
+            if guess < 1 or guess > 8:
+                 raise ValueError("Enter A Value Between 1, and 8, Try again:\n  ")
+          except ValueError:
+              print("Needs To Be a Number Above 0, Try Again:\n     ")
+
+          else:
+              print("TEST PRINT")
+              return guess
+
+def number_gen():
+    '''
+    - Create a Random Generated Number between 1 - 8
+
+    '''
+    magic_number = random.randint(1, 8)
+    if magic_number == 0:
+        magic_number += 1
+    return magic_number
+
+def one_game():
+    # STEP 1
+    print_welcome()
+    # STEP 2
+    print_guessing_game()
+    # STEP 3
+    trys = 0
+    magic_number = number_gen()
+    while True:
+
+        my_guess =prompt_user()
+
+        if my_guess < magic_number:
+            print("----------- ** TOO LOW!! ** -----------\n")
+            trys += 1
+
+        elif my_guess > magic_number:
+            print("----------- ** TOO HIGH!! ** -----------\n")
+            trys += 1
+
+        else:
+            print("""
+            =======================
+            GOOD JOB IT WAS:\n
+            {}
+            YOU GUESSED:\n
+            {}
+            WITH {} TRIES
+            ======================""".format(magic_number, my_guess, trys))
+            return magic_number
+    # STEP 4
+
+
+
+if __name__ == '__main__':
+    one_game()
